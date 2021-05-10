b45ac53f8ad0968d379f0d2e67f87e5385fd82aa
beejay321 <jibodu.busola@gmail.com>
2021-05-10 12:06:42 +0200

    Commit branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./style.css";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MyNav from "./components/MyNav.jsx";
+import Footer from "./components/Footer.jsx";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <MyNav />
+        <Footer />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
