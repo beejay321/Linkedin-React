4d2a35abc34d0df5016622bea462a63adda16419
Helena Nel <mailhedri@gmail.com>
2021-05-10 13:39:16 +0200

    Made Collapsable
    Added separate component for person.
    Added collapse feature to show more.

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,19 +1,43 @@
-import { Component } from "react"
-import "./Sidebar.css"
-import { Button, ListGroup } from "react-bootstrap"
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { Row, Col, ListGroup, Collapse } from "react-bootstrap";
+import SidebarPerson from "./SidebarPerson.jsx";
 
-class Sidebar extends Component {
-  render() {
-    return (
-      <>
-        <ListGroup>
-          <ListGroup.Item disabled>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        </ListGroup>
-      </>
-    )
-  }
+function Sidebar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <ListGroup>
+        <ListGroup.Item>
+          <h4 id="sidebar-heading">People also viewed</h4>
+          <Row>
+            <SidebarPerson />
+            <SidebarPerson />
+            <SidebarPerson />
+          </Row>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <h4
+            onClick={() => setOpen(!open)}
+            aria-controls="example-collapse-text"
+            aria-expanded={open}
+          >
+            Show More
+          </h4>
+          <Collapse in={open}>
+            <Row id="example-collapse-text">
+              <SidebarPerson />
+              <SidebarPerson />
+            </Row>
+          </Collapse>
+        </ListGroup.Item>
+      </ListGroup>
+    </>
+  );
 }
-export default Sidebar
+
+export default Sidebar;
+// Your access token for Token Based Authentication is:
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDk5MTNmYjYxOWU1ZDAwMTUxZjhmODUiLCJpYXQiOjE2MjA2NDQ4NTksImV4cCI6MTYyMTg1NDQ1OX0.fm075zxqUowsPdcnZmh_76d_SkR-rUgg6MQK86gOvm0
---
+++ src/components/Sidebar/SidebarPerson.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { Image, Row, Col, Button } from "react-bootstrap";
+
+function SidebarPerson() {
+  return (
+    <>
+      <Col xs={4}>
+        <Image
+          className="sidebar-img"
+          src="https://placeimg.com/640/480/people"
+          roundedCircle
+        />
+      </Col>
+      <Col xs={8}>
+        <span>Person 1 </span>
+        <span className="text-muted">{"\u2022"}</span>
+        <span className="text-muted"> 2nd</span>
+        <p>Cras justo odio</p>
+        <Button className="sidebar-btn text-muted" variant="outline-dark">
+          Message
+        </Button>
+      </Col>
+    </>
+  );
+}
+
+export default SidebarPerson;
+// Your access token for Token Based Authentication is:
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDk5MTNmYjYxOWU1ZDAwMTUxZjhmODUiLCJpYXQiOjE2MjA2NDQ4NTksImV4cCI6MTYyMTg1NDQ1OX0.fm075zxqUowsPdcnZmh_76d_SkR-rUgg6MQK86gOvm0
