5429637ecec8ae13ce99b5d26453717643e1ccbe
Helena Nel <mailhedri@gmail.com>
2021-05-10 12:06:06 +0200

    commit

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import "./Sidebar.css";
+import { Button, ListGroup } from "react-bootstrap";
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <>
+        <ListGroup>
+          <ListGroup.Item disabled>Cras justo odio</ListGroup.Item>
+          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+        </ListGroup>
+      </>
+    );
+  }
+}
+export default Sidebar;
