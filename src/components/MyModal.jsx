d8ad71e446b390368c6b91a01b56027c148784aa
gregoriofracassi <gregorio.fracassi@gmail.com>
2021-05-12 11:29:03 +0200

    Update MyModal.jsx

--- src/components/MyModal.jsx
+++ src/components/MyModal.jsx
@@ -1,41 +1,20 @@
-import { Button, Modal, Form } from "react-bootstrap"
-import { useState } from "react"
+import React from "react";
+import {
+  Modal,
+  Button,
+  Form,
+  FormControl,
+  Col,
+  Row,
+  InputGroup,
+} from "react-bootstrap";
+import { useState } from "react";
 
-// export default function Example() {
-//   const [show, setShow] = useState(false)
+function ModalShow() {
+  const [show, setShow] = useState(false);
 
-//   const handleClose = () => setShow(false)
-//   const handleShow = () => setShow(true)
-
-//   return (
-//     <>
-//       <div onClick={handleShow}>
-//         <i class="bi bi-plus fs-3"></i>
-//       </div>
-
-//       <Modal show={show} onHide={handleClose}>
-//         <Modal.Header closeButton>
-//           <Modal.Title>Modal heading</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={handleClose}>
-//             Close
-//           </Button>
-//           <Button variant="primary" onClick={handleClose}>
-//             Save Changes
-//           </Button>
-//         </Modal.Footer>
-//       </Modal>
-//     </>
-//   )
-// }
-
-export default function ModalShow(props) {
-  const [show, setShow] = useState(false)
-
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
@@ -49,85 +28,26 @@ export default function ModalShow(props) {
           <Modal.Title>Add Experience</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={props.handleSubmit}>
-            <Form.Group>
-              <Form.Label>Role</Form.Label>
-              <Form.Control
-                id="role"
-                type="text"
-                placeholder="Role..."
-                value={props.role}
-                onChange={props.handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Company Name</Form.Label>
-              <Form.Control
-                id="company"
-                type="text"
-                placeholder="Company Name..."
-                value={props.company}
-                onChange={props.handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Start date</Form.Label>
-              <Form.Control
-                id="startDate"
-                type="datetime-local"
-                placeholder="Start date..."
-                value={props.startDate}
-                onChange={props.handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>description</Form.Label>
-              <Form.Control
-                id="description"
-                type="text-area"
-                placeholder="description..."
-                value={props.description}
-                onChange={props.handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Area</Form.Label>
-              <Form.Control
-                id="area"
-                type="text"
-                placeholder="Area..."
-                value={props.area}
-                onChange={props.handleChange}
-              />
-            </Form.Group>
-            <Button
-              as="input"
-              type="submit"
-              value="Submit"
-              variant="outline-primary"
-            />
-          </Form>
+          <Form.Group>
+            <Form.Label>Role</Form.Label>
+            <Form.Control type="text" placeholder="role..." />
+          </Form.Group>
         </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary">Delete</Button>
+          <Button variant="primary">Save Changes</Button>
+        </Modal.Footer>
       </Modal>
     </>
-  )
+  );
 }
 
-// class ModalForm extends React.Component {
-//   state = {
-//     experience: {},
-//   }
+class ModalForm extends React.Component {
+  state = {};
 
-//   handleChange = (e) => {
-//     let id = e.target.id
-//     this.setState({
-//       experience: { ...this.state.experience, [id]: e.target.value },
-//     })
-//   }
+  render() {
+    return <ModalShow />;
+  }
+}
 
-//   render() {
-//     return <ModalShow />
-//   }
-// }
-
-// export default ModalForm
+export default ModalForm;
