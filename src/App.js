193297e1490b20e56c9c91f7aaff6514bebd91b8
gregoriofracassi <gregorio.fracassi@gmail.com>
2021-05-13 14:07:19 +0200

    implemented search

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React from "react";
-import Home from "./components/Home";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import MyNav from "./components/MyNav";
-import Footer from "./components/Footer";
-import Feeds from "./components/Feeds";
+import logo from "./logo.svg"
+import "./App.css"
+import React from "react"
+import Home from "./components/Home"
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
+import MyNav from "./components/MyNav"
+import Footer from "./components/Footer"
+import Feeds from "./components/Feeds"
+import SearchResults from "./components/SearchResults"
 
 class App extends React.Component {
-  state = {};
+  state = {
+    query: "",
+  }
+
+  handleChange = (e) => {
+    this.setState({ query: e.target.value })
+  }
 
   render() {
     return (
       <Router>
-        <MyNav />
-       
-        <Route
-          component={Feeds} path="/"
-          exact
-        />
+        <MyNav query={this.state.query} handleChange={this.handleChange} />
 
-        {/* <Route exact path="/">
-          {<Redirect to="/me" />}
-        </Route> */}
-        <Route component={Home} path="/:id" />
+        <Route component={Feeds} path="/" exact />
+        <Route path="/search/:query" component={SearchResults} />
+        <Route component={Home} path="/user/:id" />
         <Footer />
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
