47c0037d1fcff70a9012c172d1a1b0b3d40514d7
Helena Nel <mailhedri@gmail.com>
2021-05-14 16:40:50 +0200

    commit

--- src/components/FeedPage/RightColumn/RightColumnTwo.jsx
+++ src/components/FeedPage/RightColumn/RightColumnTwo.jsx
@@ -5,21 +5,37 @@ export default function RightColumnTwo() {
   return (
     <Card className="RightColumn-card RightColumnTwo-card">
       <Card.Header className="bg-white">
-        <span>Today's most viewed courses</span>
+        <span className="pr-2">Today's most viewed courses</span>
         <i class="bi bi-info-square-fill"></i>
       </Card.Header>
       <Card.Text className="m-0 p-0  px-3">
         <Card.Text>
-          <p>1. Only available for a limited time</p>
+          <p>1. The Six Morning Habitrs of High Performance</p>
+          <div id="RC2-span" className="text-muted p-0 m-0">
+            <span>Pete Mockaitis</span>
+            <span> | </span>
+            <span> How to Be Awesome...</span>
+          </div>
         </Card.Text>
         <Card.Text>
-          <p>2. Only available for a limited time</p>
+          <p className="p-0 m-0">2. Unconcscious Bias</p>
+          <div id="RC2-span" className="text-muted p-0 m-0">
+            <span>Pete Mockaitis</span>
+            <span> | </span>
+            <span> How to Be Awesome...</span>
+          </div>
         </Card.Text>
         <Card.Text>
-          <p>3. Only available for a limited time</p>
+          <p>3.Speaking Confidently and Effectivly</p>
+          <p>1. The Six Morning Habitrs of High Performance</p>
+          <div id="RC2-span" className="text-muted p-0 m-0">
+            <span>Pete Mockaitis</span>
+            <span> | </span>
+            <span> How to Be Awesome...</span>
+          </div>
         </Card.Text>
+        <hr />
         <Card.Text>
-          "plus-icon"
           <span className="text-muted">
             Show more LinkedIn Learning <i class="bi bi-arrow-right"></i>
           </span>
